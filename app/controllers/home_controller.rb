0dc6ad09bc3b123d3c201388b0fee71f9893a289
Risal Hidayat <risalhidayat88@gmail.com>
2016-05-24 15:21:52 +0700

    update homepage, load data from database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-
+    @profile = Profile.first
+    @experiences = Experience.all
+    @portfolios = Portfolio.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   resources :portfolios
   devise_for :users
   resources :experiences
+  resource :profiles
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
   
-  resource :profiles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
